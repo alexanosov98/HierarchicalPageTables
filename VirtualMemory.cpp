12e777a5d212c57636279b1945dea41c38ba8e7f
alexanosov <alex.anosov@mail.huji.ac.il>
2024-07-23 11:16:02 +0300

    1

--- VirtualMemory.cpp
+++ VirtualMemory.cpp
@@ -2,6 +2,8 @@
 #include "PhysicalMemory.h"
 
 #define OFFSET_MASK ((1LL << OFFSET_WIDTH) - 1)
+#define NOT_IN_RAM 0;
+
 
 void VMinitialize() {
 }
@@ -35,9 +37,9 @@ uint64_t decimalToBinary(uint64_t decimalNumber) {
 }
 
 /*
- * Returns in binary
+ * Returns in decimal
  */
-uint64_t nextPMAddress(uint64_t address, int depth) {
+uint64_t getNextPageOffset(uint64_t address, int depth) {
     if (!depth){
         return address << TABLES_DEPTH * OFFSET_WIDTH;
     }
@@ -45,7 +47,8 @@ uint64_t nextPMAddress(uint64_t address, int depth) {
 }
 
 
-int64_t checkForEmptyFrame(){
+word_t checkForEmptyFrame(){
+  //todo MAKE SURE WE DONT USE A FRAME WE NEED
     word_t val;
     for (uint64_t i = 0; i < NUM_FRAMES; i++){
         PMread(i*PAGE_SIZE, &val);
@@ -57,40 +60,83 @@ int64_t checkForEmptyFrame(){
 }
 
 
-int VMread(uint64_t virtualAddress, word_t *value) {
-    uint64_t offset = virtualAddress & OFFSET_MASK;
-    int treeDepth = 0;
-    uint64_t nextPhysicalAddress = 0;
-    word_t frame = 0;
-
-    while (treeDepth <= TABLES_DEPTH){
-        uint64_t pageIndex = nextPMAddress(virtualAddress, treeDepth);
-
-        nextPhysicalAddress = frame + pageIndex * PAGE_SIZE;
-
-        PMread(nextPhysicalAddress, &frame); // returns pointer
-
-        if (&frame == nullptr){
-            word_t emptyFrameAddress = checkForEmptyFrame();
-            if (!emptyFrameAddress){
-                //todo remove references to it from its parents
-            }
-
-        }
-        treeDepth++;
-    }
-
-    //Add offset.
-    nextPhysicalAddress += binaryToDecimal(offset);
-    //Extract the wanted data from the final frame.
-    PMread(nextPhysicalAddress, value);
-    if (value == nullptr){
-
-    }
+/*
+ * Returns the new frame address, evicts an existing frame if needed
+ */
+word_t findNewFrame(){
+//  if (maxFrameReached + 1 < NUM_FRAMES){
+//
+//  }
+  //todo evict, check MAX FRAME REACHED.
+  word_t empty_frame = checkForEmptyFrame();
+  if(empty_frame != 0){
+    return empty_frame;
+  }
 
 }
 
+
+
+
+int VMread(uint64_t virtualAddress, word_t *value) {
+}
+
+
 int VMwrite(uint64_t virtualAddress, word_t value) {
-    return 0;
+
+  uint64_t finalOffset = virtualAddress & OFFSET_MASK;
+  int treeDepth = 0;
+
+  uint64_t currFrame = 0;
+  word_t newFrame = 0;
+  uint64_t nextPageOffset = 0;
+
+  word_t readValue = 0;
+  uint64_t maxFrameReached = 0;
+
+  //todo update maxFrameReached
+  //Traverse through the tree.
+  while (treeDepth < TABLES_DEPTH){
+
+    nextPageOffset = getNextPageOffset (virtualAddress, treeDepth);
+
+    PMread (currFrame * PAGE_SIZE + nextPageOffset, &readValue);
+
+    //Page table is not in the RAM but the frame is empty
+    if (readValue == 0){
+      //Find a new frame to store the Page table
+      newFrame = findNewFrame ();
+      //If the next layer is a table, fill it with zeros.
+      for (int i = 0; i < PAGE_SIZE; i++){
+        PMwrite (newFrame * PAGE_SIZE + i, 0);
+      }
+      //Link the previous table to the new table
+      PMwrite (currFrame, newFrame);
+      currFrame = newFrame;
+    }
+
+    //Page table is in the RAM
+    else{
+      currFrame = newFrame;
+      newFrame = readValue;
+    }
+    treeDepth++;
+  }
+
+  //Write the value in the PM
+  PMread (currFrame * PAGE_SIZE + finalOffset, &readValue);
+  //PM Address is empty
+  if (readValue == 0){
+    //Find a new frame to store the Page table
+    newFrame = findNewFrame ();
+    //Link the previous table to the new table
+    PMwrite (currFrame, value);
+  }
+
+    //Page table is in the RAM
+  else{
+    currFrame = newFrame;
+    newFrame = readValue;
+  }
 }
 
